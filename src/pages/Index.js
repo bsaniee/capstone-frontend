ba3ccce6eddcc9de72b955e09a11ae95135941a3
Bijan Saniee <bsaniee@gmail.com>
2021-12-23 01:46:37 -0500

    Index.js edits

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,5 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
-import ImageSlider from "../components/ImageSlider"
-import { SliderData } from "../components/SliderData"
 import { Fade } from "react-awesome-reveal";
 
 
